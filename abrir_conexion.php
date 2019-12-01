8c0a435cffa67a46b6d3b170197d822f97d19268
luisalberto26 <56483785+luisalberto26@users.noreply.github.com>
2019-12-01 18:00:51 -0400

    Update abrir_conexion.php

--- abrir_conexion.php
+++ abrir_conexion.php
@@ -1,16 +1,16 @@
 <?php 
-	// Parametros a configurar para la conexion de la base de datos 
-	$host = "localhost";    // sera el valor de nuestra BD 
-	$basededatos = "propietario";    // sera el valor de nuestra BD 
-	$usuariodb = "luis";    // sera el valor de nuestra BD 
-	$clavedb = "12345";    // sera el valor de nuestra BD 
+	
+	$host = "localhost";     
+	$basededatos = "propietario";     
+	$usuariodb = "luis";     
+	$clavedb = "12345";     
 
-	//Lista de Tablas
-	$tabla_db1 = "propietario"; 	   // tabla de propietarios
-	$tabla_db2 = "usuario"; 	   // tabla de usuarios
+	
+	$tabla_db1 = "propietario"; 	   
+	$tabla_db2 = "usuario"; 	   
 	
 
-	//error_reporting(0); //No me muestra errores
+	
 	
 	$conexion = new mysqli($host,$usuariodb,$clavedb,$basededatos);
 
